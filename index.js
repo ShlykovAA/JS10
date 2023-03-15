22e4a6612428145b1e4d91b6d3e006ed9da23ffd
ShlykovAA <anatolio7z@gmail.com>
2023-03-15 17:37:31 +0200

    lesson 20 Homework

--- index.js
+++ index.js
@@ -1,188 +1,220 @@
 console.log("Hello!!!");
 
-//Return total days count
-//getDaysInYear(year);
-//getDaysInYear(2021);
-// 365
-//getDaysInYear(2020);
-// 366
+//Unique values
+// Check if word in anagrams arr is anagram
+// If you found anagram store this word in set or map
+// Values in set or map should not repeat anagram of this word
+// Also should not be present in set or map
+// Try to optimize script by using check has
+// Return collection set or map
 
-const getDaysInYear = (year) => {
-  let startDate = new Date(0);
-  const newDate = new Date(startDate.setFullYear(year))
-  const yearTime = startDate.setFullYear(year+1) - newDate;
-  const days = yearTime / 86400000;
-  return days
-}
+// Example: "actor" and "carot" are anagrams so store only "actor"
 
-console.log(getDaysInYear(2021));
-console.log(getDaysInYear(2020));
-
-//Return day number from date
-//getDayNumber(date);
-//getDayNumber("2023-01-12");
-// 12
-//getDayNumber("2023-02-26");
-// 57
-
-const getDayNumber = (date) => {
-  const startDate = new Date(date);
-  const firstDayInYear = new Date(startDate);
-  firstDayInYear.setMonth(0);
-  firstDayInYear.setDate(1);
-  const dayNumber = ((startDate - firstDayInYear) / 86400000) + 1;
-  return dayNumber
-}
-
-console.log(getDayNumber("2023-01-12"));
-console.log(getDayNumber("2023-02-26"));
-
-// Return quarter number from date
-// I quarter: Jan - Mar
-// II quarter: Apr - Jun
-// III quarter: Jul - Sep
-// IV quarter: Oct - Dec
-// getQuarters(date);
-// getQuarters("2023-02-26");
-// Feb 26 is I quarter
-
-const getQuarters = (date) => {
-  const startDate = new Date(date);
-  const monthList = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  const month = monthList[startDate.getMonth()];
-  const dayOfDate = startDate.getDate();
-  const quarterlist = {
-    'Jan':"I quarter",
-    'Feb':"I quarter",
-    'Mar':"I quarter",
-    'Apr':"II quarter",
-    'May':"II quarter",
-    'Jun':"II quarter",
-    'Jul':"III quarter",
-    'Aug':"III quarter",
-    'Sep':"III quarter",
-    'Oct':"IV quarter",
-    'Nov':"IV quarter",
-    'Dec':"IV quarter"
-  };
-  const quart = quarterlist[month];
-  return `${month} ${dayOfDate} is ${quart}`;
-}
-
-console.log(getQuarters("2023-02-26"))
-
-// Write a function to calculate date diff
-// Return value should be a formated string
-// If diff is less then hour use minutes format If diff is less then day use hour and minute format
-// If diff is less then month use days format If diff is less then year use moth format Everything else can be in years format
-// calcDateDiff(startDate, endDate);
-// calcDateDiff("2023-02-26 14:00", "2023-02-26 14:20");
-// 20 minutes
-//calcDateDiff("2023-02-26 14:00", "2023-02-26 15:30");
-// 1 hours 30 minutes
-//calcDateDiff("2023-02-26 14:00", "2023-02-28 15:30");
-// 2 days
-//calcDateDiff("2023-02-26 14:00", "2023-05-28 15:30");
-// 3 months
-//calcDateDiff("2023-02-26 14:00", "2025-05-28 15:30");
-// 2 years
-
-const calcDateDiff = (startDate, endDate) => {
-  const firstDate = new Date(startDate);
-  const secondDate = new Date(endDate);
-  const difference = secondDate - firstDate;
-  if (difference > 31536000000) {
-    return `${Math.trunc(difference / 31536000000)} years`
-  } else if (difference > 2592000000) {
-    return `${Math.trunc(difference / 2592000000)} months`
-  } else if (difference > 86400000) {
-    return `${Math.trunc(difference / 86400000)} days`
-  } else {
-    let minutes = difference / 60000;
-    if (minutes >= 60) {
-      let hours = Math.trunc(minutes / 60)
-      let rest = (minutes - (hours*60));
-      return `${hours} hours ${rest} minutes`
-    } else {
-      return `${minutes} minutes`
-    }
-  }
-}
-
-console.log(calcDateDiff("2023-02-26 14:00", "2025-05-28 15:30"));
-console.log(calcDateDiff("2023-02-26 14:00", "2023-05-28 15:30"));
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-28 15:30"));
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-26 15:30"));
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-26 14:20"));
-
-// Create map from array of objects
-// Key should be id from object
-// Please add more input data
-//createMap(arr);
-//createMap([
-//   { id: 1, value: 1, date: "2022-02-15" },
-//   { id: 2, value: 1242, date: "2023-02-15" },
-//   { id: 3, value: 3342, date: "2021-02-15" },
-// ]);
-
-const arrOfObj = [
-  { id: 1, value: 1, date: "2022-02-15" },
-  { id: 2, value: 1242, date: "2023-02-15" },
-  { id: 3, value: 3342, date: "2021-02-15" },
-  { id: 4, value: 5472, date: "2020-01-16" },
-  { id: 5, value: 8191, date: "2022-04-22" },
-  { id: 6, value: 12648, date: "2023-03-04" }
+const anagrams = [
+  "actor",
+  "carot",
+  "listen",
+  "enlist",
+  "debit",
+  "bidet",
+  "tear",
+  "rate",
+  "night",
+  "thing",
+  "lives",
+  "veils",
+  "stressed",
+  "desserts",
+  "dormitory",
+  "dirty room",
+  "rescue",
+  "secure",
+  "a gentleman",
+  "elegant man",
+  "listen",
+  "heart",
+  "angel",
+  "leader",
+  "silent",
 ];
-const map1 = new Map();
 
-for (let i = 0; i< arrOfObj.length; i++) {
-  map1.set(arrOfObj[i].id,arrOfObj[i]);
-}
+const unique = (arr) => {
+  let x;
+  let y;
+  let z;
+  let litters = new Set();
+  let uniqueAnagrams = [];
 
-console.log(map1);
+  for (let i = 0; i < arr.length; i++) {
+    x = arr[i].split('');
+    y = x.sort();
+    z = y.join("");
 
-// Create a cache for calcDateDiff
-//cacheCalcDateDiff(startDate, endDate);
-
-const map2 = new Map();
-const newCalcDateDiff = (startDate, endDate) => {
-  const cacheInfo = `${startDate} ${endDate}`;
-  let answer;
- if (map2.has(cacheInfo)) {
-  answer = map2.get(cacheInfo);
-  return answer;
- } else {
-  const firstDate = new Date(startDate);
-  const secondDate = new Date(endDate);
-  const difference = secondDate - firstDate;
-  if (difference > 31536000000) {
-    answer = `${Math.trunc(difference / 31536000000)} years`;
-    map2.set(cacheInfo, answer);
-  } else if (difference > 2592000000) {
-    answer = `${Math.trunc(difference / 2592000000)} months`;
-    map2.set(cacheInfo, answer);
-  } else if (difference > 86400000) {
-    answer = `${Math.trunc(difference / 86400000)} days`;
-    map2.set(cacheInfo, answer);
-  } else {
-    let minutes = difference / 60000;
-    if (minutes >= 60) {
-      let hours = Math.trunc(minutes / 60);
-      let rest = (minutes - (hours*60));
-      answer = `${hours} hours ${rest} minutes`;
-      map2.set(cacheInfo, answer);
-    } else {
-      answer = `${minutes} minutes`;
-      map2.set(cacheInfo, answer);
+    if (!litters.has(z)) {
+      litters.add(z);
+      uniqueAnagrams.push(arr[i]);
     }
+
   }
-  return answer;
- }
+  return uniqueAnagrams;
+
+};
+
+console.log(unique(anagrams));
+
+
+//Getter and setter
+// Create an user object
+// User should have to first name last name and age Add userInfo getter which gets all info from obj
+// Add userInfo setter to set all user info from string or obj
+// String setter format 'Taras Samoilenko 25'
+
+const user = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  get userInfo(){
+    return `${this.firstName} ${this.lastName} is ${this.age}`
+  },
+  set userInfo(a){
+    if (typeof(a) === 'string') {
+      let arrWords = a.split(" ");
+      this.firstName = arrWords[0];
+      this.lastName = arrWords[1];
+      this.age = +arrWords[2];
+    } else if (typeof(a) === 'object') {
+      this.firstName = a.firstName;
+      this.lastName = a.lastName;
+      this.age = a.age;
+    }
+  },
+};
+
+user.userInfo = "Taras Samoilenko 25";
+user.userInfo;
+// "Taras Samoilenko is 25"
+
+user.userInfo = { firstName: "Kate", lastName: "Karp", age: 22 };
+user.userInfo;
+// "Kate Karp is 22"
+
+
+//Create arr method
+// Create obj with property from and to
+// Create function createArr that reads obj properties and
+// Creates new sorted arr with range from and to
+// Assign this arr as obj property
+
+// Create 3 solutions Solution 1 without bind, call or apply
+// Solution 2 with bind Solution 3 with call or apply
+
+const obj = {
+  from: 1,
+  to: 10,
+};
+
+const createArr = function (){
+  let arrFromTo = [];
+  for (let i = this.from; i <= this.to; i++){
+    arrFromTo.push(i);
+  };
+  return arrFromTo;
+};
+
+// Sol 1
+obj.createArr = createArr;
+
+// Sol 2
+const bindedSolution = createArr.bind(obj);
+
+// Sol 3
+const callSolut = createArr.call(obj)
+
+
+//Constructor Function
+// Create a constructor function that creates car obj
+// Obj should have properties model, color, age, speed, gasTank and started 
+// Obj should have methods startEngine, drive, stop, speedUp, slowDown, addGas This methods should be chainable
+// startEngine method checks if car has gas (you can create another method for checking gas)
+// If gas is not empty set property started to true
+// drive method should do smth only if property started is true and gas is not 0
+// if started true increase speed to 30
+// stop method sets property started to false and speed to 0
+// speedUp method increases speed by arg. Max speed is 200
+// Each speedUp method decreases gas by 5. Max gas is 20 If gas is empty stop car
+// gas cannot be less then 0
+// If started is false stop car
+// speedUp(5) adds 5 to speed
+// slowDown works like speedUp but decreases speed. Min speed is 0
+// addGas method adds gas to car by arg
+// addGas(5) adds 5 to gas
+
+function Car(model, color, age, speed, gasTank, started) {
+  this.model = model;
+  this.color = color;
+  this.age = age;
+  this.speed = speed;
+  this.gasTank = gasTank;
+  this.started = started;
+  function startEngine (){
+    if (this.gasTank > 0){
+      this.started = true;
+    }
+    return this;
+  }
+  this.startEngine = startEngine;
+  function drive (){
+    if (this.started === true && this.gasTank > 0){
+      this.speed = +this.speed + 30;
+    }
+    return this;
+  }
+  this.drive = drive;
+  function stop (){
+    this.speed = 0;
+    this.started = false;
+    return this;
+  }
+  this.stop = stop;
+  function speedUp (speedUpArg){
+    this.speed = +this.speed + +speedUpArg;
+    if(this.speed > 200){
+      this.speed = 200;
+    }
+    this.gasTank = this.gasTank - 5;
+    if (this.gasTank < 0){
+      this.gasTank = 0;
+    }
+    if(this.gasTank === 0 || this.started === false){
+      this.speed = 0;
+      this.started = false;
+    }
+    return this;
+  }
+  this.speedUp = speedUp;
+  function slowDown (speedDownArg){
+    this.speed = +this.speed - +speedDownArg;
+    if (this.speed < 0){
+      this.speed = 0;
+    }
+    this.gasTank = this.gasTank - 5;
+    if (this.gasTank < 0){
+      this.gasTank = 0;
+    }
+    if(this.gasTank === 0 || this.started === false){
+      this.speed = 0;
+      this.started = false;
+    }
+    return this;
+  }
+  this.slowDown = slowDown;
+  function addGas (addGasArg){
+    this.gasTank = +this.gasTank + +addGasArg;
+    return this;
+  }
+  this.addGas = addGas;
 }
 
-console.log(newCalcDateDiff("2023-02-26 14:00", "2025-05-28 15:30"));
-console.log(newCalcDateDiff("2023-02-26 14:00", "2023-05-28 15:30"));
-console.log(newCalcDateDiff("2023-02-26 14:00", "2023-02-28 15:30"));
-console.log(newCalcDateDiff("2023-02-26 14:00", "2023-02-26 15:30"));
-console.log(newCalcDateDiff("2023-02-26 14:00", "2023-02-26 14:20"));
-console.log(map2);
+const myCar = new Car("Reno", "White", 10, 0, 20, false);
+
+console.log(myCar);
