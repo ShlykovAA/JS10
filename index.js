62fe1cb426b42d9b60e1a55464553ab70b0215a3
ShlykovAA <anatolio7z@gmail.com>
2023-03-28 01:54:49 +0300

    lesson 24 home work

--- index.js
+++ index.js
@@ -1,125 +1,140 @@
 console.log("Hello!!!");
 
-// Create class Vehicle
-// Initialize class with properties power, gasTank and mass in tones Calculate max speed by formula 0.84 * power / mass
-// Create method getMaxSpeed that returns maxSpeed
-// Calculate gas usage per km by formula Math.round(maxSpeed / power * 100)
-// Create method getGasUsage that returns gasUsage 
-// Create method startEngine that sets property started to true Create method stopEngine that sets property started to false Create method drive. This method receives 2 arguments speed and distance in km
-// You cannot drive if started is false
-// You cannot set speed more then maxSpeed and less then 0
-// Update gas property corresponding to gasUsage Formula to calculate gas level is distance * gasUsage / 100
-// Create method addGas adds gas to car by arg
-// Argument must be bigger then zero
-// You cannot pour more gas then gasTank
-// Create method printInfo that prints in console all available information
+// Create class MockServer
+// Create private property users
+// users is an array
+// Methods can be static
+// All class methods should have 1 sec of delay. Similar to what we wrote during lesson
+// Create method getAllUsers. This method returns a fulfilled promise with all users
+// Create method getUser. This method receives id as arg.
+// This method returns a fulfilled promise with user if user exists in users
+// and rejected promise with custom error if no user found
+// Create method addUser which adds users to users. This method receives user object as arg.
+// addUser returns a fulfilled promise with value true if user arg has properties id, name, age
+// else addUser returns a rejected promise with value custom error
+// Create method updateUser which updates user in users. This method receives id as first arg user object as second arg.
+// updateUser returns a fulfilled promise with updated users if user exists in users
+// else updateUser returns a rejected promise with value custom error
+// Create method deleteUser which deletes user in users. This method receives id as arg
+// deleteUser returns a fulfilled promise with value true if user existed in users
+// and was successfully deleted.
+// else deleteUser returns a rejected promise with value custom error
 
-// Create class Car
-// Car should inherit from Vehicle
-// Initialize Car with additional properties type and maxPassengerCount
-// Examples of type SEDAN, MINIVAN, SPORTS CAR...
-// Update method printInfo that prints in console all available information
+// Please write a function or directly call MockServer and handle response with promise chains. console.log the result of function call Create another function that works with async/await and console.log the result
+// Create special function that creates users from promise array
 
-// Create class Bus
-// Bus should inherit from Car Create method updatePassengers that receives argument passengers and updates passengerCount to that number
-// passengerCount cannot be more then maxPassengerCount and less then 0
-// Update method printInfo that prints in console all available information
-
-class Vehicle {
-    constructor(power, gasTank, mass){
-        this.power = power;
-        this.gasTank = gasTank;
-        this.mass = mass;
-        this.tankLevel = this.gasTank;
+class MockServer {
+    #users = [
+        {id:1,name:"Mike",age:33},
+        {id:2,name:"Ella",age:28},
+    ];
+    getAllUsers() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(this.#users)
+            }, 1000)
+        });
     }
-    started = false;
-    speed = 0;
-    maxSpeed(){
-        return 0.84 * this.power / this.mass;
+    getUser(id){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const user = this.#users.find((item) => item.id === id);
+                if (user) {
+                    resolve(user);
+                } else {
+                    reject(new Error("No user found"));
+                }
+            }, 1000)
+        })
     }
-    gasUsage(){
-        return Math.round(this.maxSpeed() / this.power * 100);
+    addUser(user){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (Object.keys(user).includes('id') && Object.keys(user).includes('name') && Object.keys(user).includes('age')) {
+                    this.#users.push(user);
+                    resolve(true);
+                } else {
+                    reject(new Error("Please write the correct object with id, name, age."))
+                }
+            }, 1000)
+        })
     }
-    getMaxSpeed(){
-        return this.maxSpeed();
+    updateUser(id,user){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const findUser = this.#users.find((item) => item.id === id);
+                if (findUser) {
+                    this.#users = this.#users.reduce((acc,item)=>{
+                        if (item.id === id) {
+                            acc.push(Object.assign(item, user))
+                        } else {
+                            acc.push(item)
+                        }
+                        return acc;
+                    },[])
+                    resolve(this.#users)
+                } else {
+                    reject(new Error("No user found"));
+                }
+            }, 1000)
+        })
     }
-    getGasUsage(){
-        return this.gasUsage();
-    }
-    startEngine(){
-        this.started = true;
-        return this;
-    }
-    stopEngine(){
-        this.started = false;
-        this.speed = 0;
-        return this;
-    }
-    drive(speed, distance){
-        if (this.started === true) {
-            this.speed = speed;
-            if (this.speed > this.maxSpeed()){
-                this.speed = this.maxSpeed();
-            } else if (this.speed < 0){
-                this.speed = 0;
-            }
-            const gasLevel = distance * this.gasUsage() / 100;
-            if (this.tankLevel - gasLevel < 0) {
-                const notEnoughGas = gasLevel - this.tankLevel;
-                this.tankLevel = 0;
-                return `Not enough ${notEnoughGas} gasoline, please use the gas station (addGas method) on the way.`
-            } else {
-                this.tankLevel = this.tankLevel - gasLevel;
-                return this;
-            }
-        } else {
-            return "Engine not running";
-        }
-    }
-    addGas(gas){
-        if (gas > 0){
-            this.tankLevel = this.tankLevel + gas;
-            if (this.tankLevel > this.gasTank){
-                this.tankLevel = this.gasTank;
-                return this;
-            }
-            return this;
-        } else {
-            return `You can't add less than zero gas.`;
-        }
-    }
-    printInfo(){
-        return `Power and mass: ${this.power}/${this.mass} Gas: ${this.tankLevel}/${this.gasTank} Current/Max speed: ${this.speed}/${this.maxSpeed()}`
+    deleteUser(id){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const findUser = this.#users.find((item) => item.id === id);
+                if (findUser) {
+                    this.#users = this.#users.reduce((acc,item)=>{
+                        if (item.id !== id) {
+                            acc.push(item)
+                        }
+                        return acc;
+                    },[])
+                    resolve(true);
+                } else {
+                    reject(new Error("No user found"));
+                }
+            }, 1000)
+        })
     }
 }
 
-class Car extends Vehicle {
-    constructor(power, gasTank, mass, type, maxPassengerCount){
-        super(power, gasTank, mass);
-        this.type = type;
-        this.maxPassengerCount = maxPassengerCount;
-    }
-    printInfo(){
-        return `Type: ${this.type} Capacity: ${this.maxPassengerCount} Power and mass: ${this.power}/${this.mass} Gas: ${this.tankLevel}/${this.gasTank} Current/Max speed: ${this.speed}/${this.maxSpeed()}`;
+const dataUsers = new MockServer();
+
+const readAllUsers = async () => {
+    const info = await dataUsers.getAllUsers();
+    console.log(info);
+};
+
+const readUserById = (id) => {
+    const info = dataUsers.getUser(id);
+    info
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+};
+
+const createsUsers = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        const newUser = dataUsers.addUser(arr[i]);
+        newUser
+        .then(() => {
+            console.log("New user added ", arr[i]);
+        })
+        .catch((err) => {
+            console.log("Incorrect user", arr[i], err);
+        })
     }
 }
 
-class Bus extends Car {
-    passengerCount = 0;
-    updatePassengers(passengers) {
-        this.passengerCount = passengers;
-        if(this.passengerCount < 0) {
-            this.passengerCount = 0;
-            return "Passengers cannot be less than zero";
-        } else if (this.passengerCount > this.maxPassengerCount) {
-            this.passengerCount = this.maxPassengerCount
-            return `Maximum number of passengers is ${this.maxPassengerCount}`;
-        }
-        return this;
-    }
-    printInfo(){
-        return `Type: ${this.type} | Passenger now/max: ${this.passengerCount}/${this.maxPassengerCount} | Power and mass: ${this.power}/${this.mass} | Gas: ${this.tankLevel}/${this.gasTank} | Current/Max speed: ${this.speed}/${this.maxSpeed()}`
-    }
-}
+createsUsers([
+    {id:5,name:"Lola",age:19},
+    {id:6,name:"Erik",age:18},
+    {id:7,name:"Gven",age:27},
+    {id:8,name:"Tom",age:20},
+    {id:9, name: "oops"}
+])
 
-const man = new Bus(270, 60, 4, "Bus", 44);
