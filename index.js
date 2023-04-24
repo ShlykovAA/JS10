e40275f085861453e61c6337dd93c44b3d93366e
ShlykovAA <anatolio7z@gmail.com>
2023-04-24 21:24:19 +0300

    lesson 27 HomeWork

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 console.log("Hello!!!");
 
-const btn = document.getElementById('btn');
-const div1 = document.getElementById('testdiv');
-const div2 = document.getElementById('div2');
+// Fetch github users https://api.github.com/users
 
-const onClick = (event) => {
-    console.log(event);
+const mainTegForCards = document.getElementById("test_div");
+
+const loadGitHubUsers = async () => {
+  const response = await fetch("https://api.github.com/users");
+  const dataFromGitHub = await response.json();
+  renderCardForUsers(dataFromGitHub);
+}
+
+const renderCardForUsers = (arrayDataUsers) => {
+  for (let i = 0; i < arrayDataUsers.length; i++) {
+    let currentUser = arrayDataUsers[i];
+    const newCardContainer = document.createElement("div");
+    const divForAvatarAndLogin = document.createElement("div");
+    const photo = avatarForCard(currentUser);
+    const login = loginForCard(currentUser);
+    const gitHubAccount = accountUrl(currentUser);
+
+    divForAvatarAndLogin.append(photo);
+    divForAvatarAndLogin.append(login);
+    divForAvatarAndLogin.classList.add("avatar_and_login_container");
+
+    newCardContainer.append(divForAvatarAndLogin);
+    newCardContainer.append(gitHubAccount);
+    newCardContainer.classList.add("card_container");
+
+    mainTegForCards.append(newCardContainer);
+  }
 };
 
-btn.addEventListener('click', onClick);
-div1.addEventListener('click', onClick);
-div2.addEventListener('click', onClick);
+const avatarForCard = (objectUser) => {
+  const newTegForPhoto = document.createElement("img");
+  newTegForPhoto.src = objectUser.avatar_url;
+  newTegForPhoto.classList.add("card_avatar");
+  return newTegForPhoto;
+};
 
+const loginForCard = (objectUser) => {
+  const newTegForLogin = document.createElement("p");
+  newTegForLogin.innerText = objectUser.login;
+  newTegForLogin.classList.add("card_login");
+  return newTegForLogin;
+};
+
+const accountUrl = (objectUser) => {
+  const newTegForUrl = document.createElement("div");
+  newTegForUrl.innerHTML = `<a href="${objectUser.html_url}" target="_blank" class="url_account">${objectUser.html_url}</a>`;
+  newTegForUrl.classList.add("card_url");
+  return newTegForUrl;
+}
+
+loadGitHubUsers();
